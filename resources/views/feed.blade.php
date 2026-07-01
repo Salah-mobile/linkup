25808f30c7ba4eb814e48680bdfa102980e2d3e0
Salah-mobile <tabitsalah12@gmail.com>
2026-07-01 12:25:10 +0100

    do some updates in the project

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -68,7 +68,7 @@
 
                         @can('update', $post)
                         <div class="flex items-center space-x-1 border-l border-slate-100 pl-2">
-                            <a href="{{route("update.page.poste",$post->id)}}"
+                            <a href="{{route("update.page.poste",$post)}}"
                                class="p-1.5 text-slate-400 hover:text-blue-600 hover:bg-slate-50 rounded-lg transition duration-150"
                                title="Modifier">
                                 <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
@@ -82,7 +82,7 @@
                                 <button type="submit"
                                         class="p-1.5 text-slate-400 hover:text-red-600 hover:bg-red-50 rounded-lg transition duration-150"
                                         title="Supprimer"
-                                        onclick="return confirm('Bghiti tmseh had l-post?')">
+                                        >
                                     <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
                                     </svg>
